823f730ecd959a7f070eacf42789f63305d62458
Rama Krishna <siddarth21@hotmail.com>
2020-04-04 23:33:51 +0800

    Updated API to filter based on name and swaras

--- raagaapi/models.py
+++ raagaapi/models.py
@@ -1,5 +1,26 @@
 from django.db import models
 
+class RagaManager(models.Manager):
+    def filter_swaras(self, swaras):
+        iter_qs = self.get_queryset()
+        filter_qs = self.get_queryset()
+
+        for raga in iter_qs:
+            if not raga.has_swaras(swaras):
+                filter_qs = filter_qs.exclude(id=raga.id)
+
+        return filter_qs
+
+    def filter_swaras_queryset(self, swaras, queryset):
+        iter_qs = queryset
+        filter_qs = queryset
+
+        for raga in iter_qs:
+            if not raga.has_swaras(swaras):
+                filter_qs = filter_qs.exclude(id=raga.id)
+
+        return filter_qs
+
 class Raga(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100, unique=True, blank=False, null=False)
@@ -8,5 +29,28 @@ class Raga(models.Model):
     arohanam = models.CharField(max_length=100, blank=False, null=False)
     avarohanam = models.CharField(max_length=100, blank=False, null=False)
 
+    objects = RagaManager()
+
     def __str__(self):
         return self.name
+
+    # Returns a list of swaras in the arohanam
+    def get_aro_swaras(self):
+        return self.arohanam.split(" ")
+
+    # Returns a list of swaras in the avarohanam
+    def get_ava_swaras(self):
+        return self.avarohanam.split(" ")
+
+    # Returns a list of swaras in both the arohanam & avarohanam
+    def get_swaras(self):
+        aro = set(self.arohanam.split(" "))
+        ava = set(self.avarohanam.split(" "))
+        return list(aro | ava)
+
+    # Checks if a string of space-seperated swaras are contained in a raga
+    # Returns boolean
+    def has_swaras(self, swaras):
+        filter_swaras = set(swaras.split(" "))
+        all_swaras = set(self.get_swaras())
+        return filter_swaras.issubset(all_swaras)
--- raagaapi/views.py
+++ raagaapi/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, filters
 
 from .serializers import RagaSerializer
 from .models import Raga
 
+
+class SwaraFilterBackend(filters.BaseFilterBackend):
+    """
+    Filter that only allows users to see their own objects.
+    """
+    def filter_queryset(self, request, queryset, view):
+        swaras = request.query_params.get('swaras', None)
+        if swaras:
+            return Raga.objects.filter_swaras_queryset(swaras, queryset)
+        else:
+            return queryset
+
+    def to_html(self, request, queryset, view):
+        pass
 
 # ragas/ => Returns all ragas
 # ragas/?search=char => Returns ragas starting with search string
@@ -15,6 +29,6 @@ class RagaViewSet(viewsets.ModelViewSet):
     """
     queryset = Raga.objects.all().order_by('format_name')
     serializer_class = RagaSerializer
-    # filter_backends = (filters.SearchFilter, SwaraFilterBackend, )
-    # search_fields = ['^format_name', '^name']
-    # filter_fields = ['swaras']
+    filter_backends = (filters.SearchFilter, SwaraFilterBackend, )
+    search_fields = ['^format_name', '^name']
+    filter_fields = ['swaras']
