6e934bc01fa480a5ce42e9e934e128765e00951e
Rama Krishna <siddarth21@hotmail.com>
2020-04-04 18:23:02 +0800

    Setup django project

---
+++ raagaapi/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Raga(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, unique=True, blank=False, null=False)
+    format_name = models.CharField(max_length=100, blank=False, null=False, default="")
+    melakarta = models.ForeignKey('Raga', null=True)
+    arohanam = models.CharField(max_length=100, blank=False, null=False)
+    avarohanam = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
